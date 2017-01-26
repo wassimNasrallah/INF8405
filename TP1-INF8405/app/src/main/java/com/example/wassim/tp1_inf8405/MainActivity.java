edf43bc7e65b5537497d1c3d048a6340c1296e91
Wassim <wassim.nasrallah.36@gmail.com>
2017-01-26 17:54:53 -0500

    init

---
+++ TP1-INF8405/app/src/main/java/com/example/wassim/tp1_inf8405/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wassim.tp1_inf8405;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
