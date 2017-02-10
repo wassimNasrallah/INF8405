0c1b9fec06d7bd96c7e890d10665870cd14bdb0d
Wassim <wassim.nasrallah.36@gmail.com>
2017-02-10 11:50:42 -0500

    fix first screen crash

--- TP1-INF8405/app/src/main/java/com/example/wassim/tp1_inf8405/MainActivity.java
+++ TP1-INF8405/app/src/main/java/com/example/wassim/tp1_inf8405/MainActivity.java
@@ -1,41 +1,55 @@
 package com.example.wassim.tp1_inf8405;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button rules;
+    private Button start;
+    private Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
 
+        butListener();
+    }
 
-        Button quit = (Button) findViewById(R.id.buttonQuit);
+    public void butListener () {
+
+        rules = (Button) findViewById(R.id.buttonRules);
+        rules.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), RulesClass.class);
+                startActivity(myIntent);
+            }
+        });
+
+        start = (Button) findViewById(R.id.buttonStart);
+        start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), LevelSelect.class);
+                startActivity(myIntent);
+            }
+        });
+
+        quit = (Button) findViewById(R.id.buttonQuit);
         quit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
             public void onClick(View view) {
                 System.exit(0);
             }
         });
-
-        Button rules = (Button) findViewById(R.id.buttonRules);
-        rules.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), RulesClass.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
-
-        Button start = (Button) findViewById(R.id.buttonStart);
-        start.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), LevelSelect.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
     }
-
 }
